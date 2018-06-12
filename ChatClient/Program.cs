7be9a1f7e5b0011115da46ae6b55d0eccb92e9fe
Mikhailiuk Kseniya <mikhailiuk.k@gmail.com>
2018-06-12 19:32:29 +0300

    TCP client

---
+++ ChatClient/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static string UserName;
+        private const string host = "127.0.0.1";
+        private const int port = 8888;
+        static TcpClient client;
+        static NetworkStream stream;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            UserName = Console.ReadLine();
+            client = new TcpClient();
+
+            try
+            {
+                client.Connect(host, port);
+                stream = client.GetStream();
+
+                string message = UserName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+                receiveThread.Start();
+                Console.WriteLine("Welcome ", UserName);
+                SendMessage();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+        }
+
+        static void SendMessage()
+        {
+            Console.WriteLine("Enter your message: ");
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+
+        static void ReceiveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, data.Length));
+                    } while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message);
+                }
+                catch
+                {
+                    Console.WriteLine("connection failed");
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+
+        static void Disconnect()
+        {
+            if (stream != null)
+            {
+                stream.Close();
+            }
+            if (client != null)
+            {
+                client.Close();
+            }
+            Environment.Exit(0);
+        }
+    }
+}
